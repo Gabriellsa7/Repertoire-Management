0083042aad1e319e95297a1db73f0b8a1dd6c431
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-12-04 01:06:57 -0300

    feat: Created the user entity

---
+++ src/main/java/com/academico/webproject/model/User.java
@@ -0,0 +1,32 @@
+package com.academico.webproject.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.UUID)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "is_leader")
+    private Boolean is_leader;
+}
+
