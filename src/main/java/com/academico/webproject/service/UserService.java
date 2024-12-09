101c4fa655605e307279570ba12242df5d1c748e
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-12-08 23:23:04 -0300

    feat: the set and getter is not working

--- src/main/java/com/academico/webproject/service/UserService.java
+++ src/main/java/com/academico/webproject/service/UserService.java
@@ -1,38 +1,38 @@
-package com.academico.webproject.service;
+    package com.academico.webproject.service;
 
-import com.academico.webproject.model.User;
-import com.academico.webproject.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+    import com.academico.webproject.model.User;
+    import com.academico.webproject.repository.UserRepository;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+    import java.util.Optional;
 
-@Service
-public class UserService {
+    @Service
+    public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+        @Autowired
+        private UserRepository userRepository;
 
-    public User createUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public Optional<User> getUserById(String id) {
-        return  userRepository.findById(id);
-    }
-
-    public void deleteUser(String id) {
-        userRepository.deleteById(id);
-    }
-
-    public User updateUser(String id, User userDetails) {
-        //if the user is found it goes to the map if not it goes to orElse.
-        //with the set, change the current values to a new.
-        return userRepository.findById(id).map(user -> {
-            user.setName(userDetails.getName());
-            user.setEmail(userDetails.getEmail());
-            user.setPassword(userDetails.getPassword());
+        public User createUser(User user) {
             return userRepository.save(user);
-        }).orElseThrow(() -> new RuntimeException("User not found"));
+        }
+
+        public Optional<User> getUserById(String id) {
+            return  userRepository.findById(id);
+        }
+
+        public void deleteUser(String id) {
+            userRepository.deleteById(id);
+        }
+
+        public User updateUser(String id, User userDetails) {
+            //if the user is found it goes to the map if not it goes to orElse.
+            //with the set, change the current values to a new.
+            return userRepository.findById(id).map(user -> {
+                user.setName(userDetails.getName());
+                user.setEmail(userDetails.getEmail());
+                user.setPassword(userDetails.getPassword());
+                return userRepository.save(user);
+            }).orElseThrow(() -> new RuntimeException("User not found"));
+        }
     }
-}
