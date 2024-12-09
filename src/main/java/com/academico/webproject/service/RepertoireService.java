3a63db3a1e3e26f0d0303eef0964cb25d42151d3
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-12-09 18:43:47 -0300

    feat: Created the Repertoire Service

---
+++ src/main/java/com/academico/webproject/service/RepertoireService.java
@@ -0,0 +1,39 @@
+package com.academico.webproject.service;
+
+import com.academico.webproject.model.Repertoire;
+import com.academico.webproject.repository.RepertoireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class RepertoireService {
+    //Methods: createRepertoire, updateRepertoire, deleteRepertoire
+    //getRepertoireById, syncRepertoire
+
+    @Autowired
+    private RepertoireRepository repertoireRepository;
+
+    public Repertoire createRepertoire(Repertoire repertoire) {
+        return repertoireRepository.save(repertoire);
+    }
+
+    public Repertoire updateRepertoire(String id, Repertoire repertoireDetails) {
+        return  repertoireRepository.findById(id).map(repertoire -> {
+            repertoire.setName(repertoireDetails.getName());
+            repertoire.setDescription(repertoire.getDescription());
+            repertoire.setBand(repertoire.getBand());
+            //think of a way to put the musicLinks here
+            return repertoireRepository.save(repertoire);
+        }).orElseThrow(() -> new RuntimeException("Repertoire not found"));
+    }
+
+    public void deleteRepertoire(String id) {
+        repertoireRepository.deleteById(id);
+    }
+
+    public Optional<Repertoire> getRepertoireById(String id) {
+        return repertoireRepository.findById(id);
+    }
+
+    //think of a way to create the syncRepertoire here
+}
