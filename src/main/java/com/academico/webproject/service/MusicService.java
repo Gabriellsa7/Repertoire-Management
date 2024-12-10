b5594404f864ff6bb82ba3f3d92fe2f573890716
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-12-10 16:32:19 -0300

    feat: created the MusicService class

---
+++ src/main/java/com/academico/webproject/service/MusicService.java
@@ -0,0 +1,45 @@
+package com.academico.webproject.service;
+
+import com.academico.webproject.model.Music;
+import com.academico.webproject.repository.MusicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MusicService {
+    @Autowired
+    private MusicRepository musicRepository;
+
+    public Music createMusic(Music music) {
+        return  musicRepository.save(music);
+    }
+
+    public Optional<Music> getMusicById(String MusicId) {
+        return musicRepository.findById(MusicId);
+    }
+
+    public List<Music> getAllMusics() {
+        return musicRepository.findAll();
+    }
+
+    public Music updateMusic(String id, Music updateMusic) {
+        return musicRepository.findById(id).map(music -> {
+            music.setTitle(updateMusic.getTitle());
+            //Search about how to send PDF to site
+            music.setRepertoireLinks(updateMusic.getRepertoireLinks());
+            return musicRepository.save(music);
+        }).orElseThrow(() -> new RuntimeException("Music not found"));
+    }
+
+    public void deleteBand(String id) {
+        if (musicRepository.existsById(id)) {
+            musicRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Music not found with ID: " + id);
+        }
+    }
+}
+
