6b82ae08b1ea20465322dae370823195ca1c9012
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-12-15 00:33:49 -0300

    feat: created the Band controller

---
+++ src/main/java/com/academico/webproject/controller/BandController.java
@@ -0,0 +1,74 @@
+package com.academico.webproject.controller;
+
+
+import com.academico.webproject.model.Band;
+import com.academico.webproject.model.User;
+import com.academico.webproject.service.BandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/band")
+public class BandController {
+
+    @Autowired
+    private BandService bandService;
+
+    @PostMapping
+    public ResponseEntity<Band> createBand(@RequestBody Band band) {
+        Band createdBand = bandService.createBand(band);
+        return ResponseEntity.ok(createdBand);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Band>> getAllBands() {
+        List<Band> bands = bandService.getAllBands();
+        return ResponseEntity.ok(bands);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Band> getBandById(@PathVariable String id) {
+        return bandService.getBandById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Band> updateBand(@PathVariable String id, @RequestBody Band updatedBand) {
+        try {
+            Band band = bandService.updateBand(id, updatedBand);
+            return ResponseEntity.ok(band);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBand(@PathVariable String id) {
+        bandService.deleteBand(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{bandId}/add-member/{userId}")
+    public ResponseEntity<Band> addMemberToBand(@PathVariable String bandId, @PathVariable String userId) {
+        Band updatedBand = bandService.addMemberToBand(bandId, userId);
+        return ResponseEntity.ok(updatedBand);
+    }
+
+    @DeleteMapping("/{bandId}/remove-member/{userId}")
+    public ResponseEntity<Band> removeMemberFromBand(@PathVariable String bandId, @PathVariable String userId) {
+        Band updatedBand = bandService.removeMemberFromBand(bandId, userId);
+        return ResponseEntity.ok(updatedBand);
+    }
+
+    @GetMapping("/{bandId}/members")
+    public ResponseEntity<List<User>> getMembersByBandId(@PathVariable String bandId) {
+        List<User> members = bandService.getMemberByBandId(bandId);
+        return ResponseEntity.ok(members);
+    }
+}
